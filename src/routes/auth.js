43a0d3cec554b60c5f3fbb514cc8a0de889c5c05
aderahmad7 <aderahmad430@gmail.com>
2025-06-28 13:50:06 +0800

    cek ok

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,6 +4,9 @@ const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.get("/tes", (req, res) => {
+  res.send("Route GET /api/auth/tes OK");
+});
 router.get("/validate-token", authenticateToken, (req, res) => {
   res.status(200).json({ message: "Token valid", user: req.user });
 });
